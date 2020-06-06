3b2d58c2ebea44c973c1bf27d3d838747fd96314
Brendan <eggers.brendan@gmail.com>
2020-06-06 18:00:58 -0400

    Python is so easy!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from sys import argv
+from os.path import exists
 # script is the file being called
 # each one after is the available arguement passed
 # script, butts, second, third = argv
@@ -7,9 +8,28 @@ from sys import argv
 # txt = open(f"{filename}.txt")
 # print(txt.read())
 
-# "w" in next line means write
 # target = open(filename, "w")
+# "w" = write "r" = read "a" = append
 # target.truncate()
 # target.write("text")
 # target.close()
 # last line finishes it
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+
+indata = open(from_file).read()
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright all done")
+
+out_file.close()
