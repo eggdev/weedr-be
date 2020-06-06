0ad4a1af0ba3689468f0b38b44283b357d96d7bd
Brendan <eggers.brendan@gmail.com>
2020-06-06 17:48:13 -0400

    Learning Python

---
+++ app.py
@@ -0,0 +1,15 @@
+from sys import argv
+# script is the file being called
+# each one after is the available arguement passed
+# script, butts, second, third = argv
+# You can redefine first to butts and have that variable defined
+# Open uses a string to find the filename. Can hard code extension
+# txt = open(f"{filename}.txt")
+# print(txt.read())
+
+# "w" in next line means write
+# target = open(filename, "w")
+# target.truncate()
+# target.write("text")
+# target.close()
+# last line finishes it
