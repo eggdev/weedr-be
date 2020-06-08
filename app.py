572741f7a27f52a0fe915ba8eb33ab3be7643087
Brendan <eggers.brendan@gmail.com>
2020-06-08 15:50:53 -0400

    This grabs the reported users in the modqueue for comments/posts

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ from config import CLIENT_ID, CLIENT_SECRET, USER_AGENT, USERNAME, PASSWORD
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
-    user_agent=USER_AGENT)
+    user_agent=USER_AGENT,
+    username=USERNAME,
+    password=PASSWORD)
 
-for submission in reddit.subreddit("learnpython").hot(limit=10):
-    print(submission.title)
+subreddit = reddit.subreddit("knicklejerk")
+
+reported_comments = subreddit.mod.reports("comments")
+reported_posts = subreddit.mod.reports("submissions")
+
+for comment in reported_comments:
+    # for reported comment in modqueue:
+    # get the reported users name
+    print(f"Comment: {comment.author}")
+
+for post in reported_posts:
+    # for reported post in modqueue:
+    # get the reported users name
+    print(f"Post: {post.author}")
