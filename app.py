48726293355358d9c4c3abe78846f50b2691a56e
Brendan <eggers.brendan@gmail.com>
2020-06-09 14:28:41 -0400

    Restarted the project following tutorial

--- app.py
+++ app.py
@@ -1,46 +1,24 @@
-from flask import Flask, request, Response
-# from models import User
-from db_config import initialize_db
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/redditor_reports'
-}
 
-initialize_db(app)
+users = [
+    {"user_name": "clydeedgar",
+     "reports": [
+         {"title": "butts"}
+     ]
+     },
+    {"user_name": "schildkrotes",
+     "reports": [
+         {"title": "carlton"}
+     ]
+     }
+]
 
 
 @app.route("/users")
-def get_users():
-    return {"user": "username"}
-
-
-@app.route("/users", methods=['POST'])
-def add_user():
-    print(request.get_json())
-    return {"body": "none"}, 200
-    # body = request.get_json()
-    # user = User(**body).save()
-    # id = user.id
-    # return {"id": str(id)}, 200
-
-
-@app.route("/users/<id>", methods=["PUT"])
-def update_user(id):
-    print(id)
-    return "", 200
-    # body = request.get_json()
-    # # Spread operator in python == **
-    # User.objects.get(id=id).update(**body)
-    # return "", 200
-
-
-@app.route("/users/<id>", methods=["DELETE"])
-def remote_user(id):
-    print(id)
-    return "", 200
-    # User.objects.get(id=id).delete()
-    # return "", 200
+def get_all_users():
+    return jsonify(users)
 
 
 app.run()
--- db_config.py
+++
@@ -1,8 +0,0 @@
-# {DB_HOST}://{DB_USERNAME}:{DB_PASSWORD}@{DB_PORT}/{DB_COLLECTION}/{SUBREDDIT}?retryWrites=true&w=majority
-from flask_mongoengine import MongoEngine
-
-db = MongoEngine()
-
-
-def initialize_db(app):
-    db.init_app(app)
