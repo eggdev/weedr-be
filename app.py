c979732ea20a07db1b6433ec02500252648bc9e7
Brendan <eggers.brendan@gmail.com>
2020-06-08 23:59:43 -0400

    Nesting and keeping the reddit object

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from helpers import get_reported_data, check_users_and_add_reports
 from config import subreddit, reddit
 from user import User
 
-# Pop into the subreddit, grab actively reported posts, and store them in JSON
-curr_reports = get_reported_data(subreddit)
-
 # TODO This becomes the response from the database
 reported_users = []
 
+# Pop into the subreddit, grab actively reported posts, and store them in JSON
+curr_reports = get_reported_data(subreddit)
 check_users_and_add_reports(curr_reports, reported_users)
 
 
+for bad_guys in reported_users:
+    print(bad_guys.name, bad_guys.redditor.name)
 # Alright now we have users being turned into custom objects
 # Those users have multiple submissions in their list of reports
 
--- user.py
+++ user.py
@@ -1,10 +1,8 @@
 class User(object):
     def __init__(self, redditor):
         self.id = redditor.id
-        self.comment_karma = redditor.comment_karma
-        self.link_karma = redditor.link_karma
-        self.is_mod = redditor.is_mod
         self.name = redditor.name
+        self.redditor = redditor
         self.reported_items = []
         self.reported_by_mod_count = 0
         self.reported_by_user_count = 0
@@ -12,5 +10,5 @@ class User(object):
     def add_reported_item(self, item):
         # if !(reported_users.reduce((usr) => usr.id === item.author.id));
         # If the passed item does not exist in the users already reported list
-        if not any(already_reported.id == item.id for already_reported in self.reported_items):
+        if not any(prev_reported.id == item.id for prev_reported in self.reported_items):
             self.reported_items.append(item)
