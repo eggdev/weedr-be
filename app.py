f8a6e28d9f752521dad6c9e36e17b627e1c16db9
Brendan <eggers.brendan@gmail.com>
2020-06-09 11:50:38 -0400

    Adding in running flask app with hot-reloading

--- app.py
+++ app.py
@@ -10,36 +10,37 @@ app.config['MONGODB_SETTINGS'] = {
 initialize_db(app)
 
 
-@app.route("/")
-def hello():
-    return {"hello": "world"}
-
-# @app.route("/users")
-# def get_users():
-#     users = User.objects().to_json()
-#     return Response(users, mimetype="application/json", status=200)
+@app.route("/users")
+def get_users():
+    return {"user": "username"}
 
 
-# @app.route("/users", methods=['POST'])
-# def add_user():
-#     body = request.get_json()
-#     user = User(**body).save()
-#     id = user.id
-#     return {"id": str(id)}, 200
+@app.route("/users", methods=['POST'])
+def add_user():
+    print(request.get_json())
+    return {"body": "none"}, 200
+    # body = request.get_json()
+    # user = User(**body).save()
+    # id = user.id
+    # return {"id": str(id)}, 200
 
 
-# @app.route("/users/<id>", methods=["PUT"])
-# def update_user(id):
-#     body = request.get_json()
-#     # Spread operator in python == **
-#     User.objects.get(id=id).update(**body)
-#     return "", 200
+@app.route("/users/<id>", methods=["PUT"])
+def update_user(id):
+    print(id)
+    return "", 200
+    # body = request.get_json()
+    # # Spread operator in python == **
+    # User.objects.get(id=id).update(**body)
+    # return "", 200
 
 
-# @app.route("/users/<id>", methods=["DELETE"])
-# def remote_user(id):
-#     User.objects.get(id=id).delete()
-#     return "", 200
+@app.route("/users/<id>", methods=["DELETE"])
+def remote_user(id):
+    print(id)
+    return "", 200
+    # User.objects.get(id=id).delete()
+    # return "", 200
 
 
 app.run()
